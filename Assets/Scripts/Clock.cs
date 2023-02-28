ff8998d00cc0d8d032884b32d91ffac2bf6e3aae
kirillyakubov101 <59371020+kirillyakubov101@users.noreply.github.com>
2023-02-28 17:53:24 -0500

    Exercise 2
    The clock is a prefab, dynamically instantiated in the scene at startup.
    You can configure in the scene the number of clocks that will be instantiated at startup.
    The clocks are distributed in space so as not to overlap

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-namespace WallClock
+namespace WallClock.Core
 {
     public class Clock : MonoBehaviour
     {
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+namespace WallClock.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Header("Clock Creation")]
+        [SerializeField] private Clock m_ClockPrefab;
+        [SerializeField] private int m_clockToCreateAtStart = 3;
+        [SerializeField] private float m_offsetX;
+
+        [Header("Camera-Target")]
+        [SerializeField] private CinemachineTargetGroup m_cinemachineTarget;
+
+        private Vector3 m_StartPosition = Vector3.zero;
+        private const float c_TargetGroupRadius = 2f;
+      
+
+        private void Start()
+        {
+            CreateClocks();
+        }
+
+        private void CreateClocks()
+        {
+            for (int i = 0; i < m_clockToCreateAtStart; i++)
+            {
+                Clock instance = Instantiate(m_ClockPrefab, m_StartPosition, Quaternion.identity);
+                m_cinemachineTarget.AddMember(instance.transform, 1f, c_TargetGroupRadius);
+
+                m_StartPosition.x += m_offsetX;
+            }
+        }
+    }
+
+}
